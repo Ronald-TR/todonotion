a31877622603f4d84011f0021e64ea50594b9db1
Ronald Rodrigues Farias <ronald-farias@outlook.com>
2021-09-23 22:27:05 -0300

    refactor: set log fatal in mapcardsby error

--- cmd/ls.go
+++ cmd/ls.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 
 	entity "github.com/ronald-tr/todonotion/entity"
 	"github.com/spf13/cobra"
@@ -18,13 +19,16 @@ var (
 		Run: func(cmd *cobra.Command, args []string) {
 			fmt.Printf("%s Cards:\n\n", lsLane)
 
-			client.MapCardsBy(func(card *entity.Card) {
+			err := client.MapCardsBy(func(card *entity.Card) {
 				fmt.Printf("%s @%s\n", card.Title, card.Property)
 			},
 				entity.CustomFilter{Type: entity.FilterTypeLane, Value: lsLane},
 				entity.CustomFilter{Type: entity.FilterTypeProperty, Value: lsProperty},
 				entity.CustomFilter{Type: entity.FilterTypeTitle, Value: lsTitle},
 			)
+			if err != nil {
+				log.Fatalln(err)
+			}
 		},
 	}
 )
