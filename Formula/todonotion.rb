afe3432ebd7170cd86c511744ae00bdc7afdca5a
Ronald <ronald-farias@outlook.com>
2022-12-26 19:04:55 -0300

    Update todonotion.rb

--- Formula/todonotion.rb
+++ Formula/todonotion.rb
@@ -3,7 +3,7 @@ require "formula"
 class Rdpcli < Formula
     desc "A todonotion CLI to improve productivity"
     homepage "https://github.com/Ronald-TR/todonotion"
-    url "https://github.com/Ronald-TR/todonotion/releases/download/v0.1.0/todonotion-mac-arm64.tar.gz",
+    url "https://github.com/Ronald-TR/todonotion/releases/download/v0.1.0/todonotion-mac-arm64.tar.gz"
     sha256 "6d49c7975fadde47f4a00d5749ce1bf1c3dbe3d1d8d33be310c8e54cc1041a24"
     head "git@github.com:Ronald-TR/todonotion.git"
   
